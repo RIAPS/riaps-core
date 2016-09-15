6ba3d84adf095e3dd6cb1765ac1b819e0918146e
Istvan Madari <istvan.madari@vanderbilt.edu>
2016-09-15 14:49:34 -0500

    - deploy script updated

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,22 +2,15 @@
 
 echo COPY to BBB1
 scp ../build/Debug/actor_pub \
-    ../build/Debug/librdiscovery.so \
-    ../build/Debug/libpublisher_component.so \
-    ../build/Debug/riaps_discoveryd_armhf \
-    ../build/Debug/pub_conf.json \
-    ../build/Debug/libzcm.so \
-    ~/Work/dependencies/libzmq/armhf/lib/libzmq.so.5 \
+    ../build/Debug/actor_sub \
+    ../build/Debug/libriaps.so \
+    ../build/Debug/rdiscoveryd \
     root@bbb1.local:/root \
 
 
 echo COPY to BBB2
-scp ../build/Debug/actor_sub \
-    ../build/Debug/librdiscovery.so \
-    ../build/Debug/riaps_discoveryd_armhf \
-    ../build/Debug/sub_conf.json \
-    ../build/Debug/libsubscriber_component.so \
-    ../build/Debug/libzcm.so \
-    ~/Work/dependencies/libzmq/armhf/lib/libzmq.so.5 \
+scp ../build/Debug/actor_pub \
+    ../build/Debug/actor_sub \
+    ../build/Debug/libriaps.so \
+    ../build/Debug/rdiscoveryd \
     root@bbb2.local:/root \
-
