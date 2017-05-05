67763eb6e68824780bbb8c9c115ac03e4e27307e
istvan <istvan.madari@vanderbilt.edu>
2017-05-05 16:40:47 -0500

    start_device skaleton added

---
+++ src/device/r_device.cc
@@ -0,0 +1,23 @@
+//
+// Created by istvan on 5/5/17.
+//
+
+
+#include <czmq.h>
+#include <string>
+#include <iostream>
+#include <framework/rfw_network_interfaces.h>
+
+
+int main() {
+    std::cout << "Starting RIAPS DEVICE " << std::endl;
+
+
+//    zactor_t *r_actor = zactor_new(devm_zactor, NULL);
+//
+//    while (!zsys_interrupted){
+//        zclock_sleep(100);
+//    }
+//
+//    zactor_destroy(&r_actor);
+}
