a341372b59c37157de7634b14a1e9b6833470c05
istvan <istvan.madari@vanderbilt.edu>
2017-09-22 16:56:01 -0500

    Group configurations added

--- include/componentmodel/r_configuration.h
+++ include/componentmodel/r_configuration.h
@@ -12,12 +12,26 @@
 
 using namespace riaps::componentmodel;
 
-//// New part
-
-struct _component_port_config {
+/**
+ * Baseclass for component and group ports. Contains portName - messageType mapping.
+ */
+struct _port_config_base {
     std::string portName;
-    bool        isLocal;
     std::string messageType;
+};
+
+/**
+ * Port-message type mapping for groups
+ */
+struct _group_port_config : public _port_config_base{
+};
+
+/**
+ * Port-message type mapping for groups
+ * Component port can be local.
+ */
+struct _component_port_config : public _port_config_base {
+    bool        isLocal;
 
     _component_port_config(){
         isLocal=false;
@@ -62,6 +76,25 @@ struct _component_ports {
     std::vector<_component_port_ins> inss;
 };
 
+/**
+ * List of ports for group types
+ * Only pub/sub
+ */
+typedef struct _component_port_pub _group_port_pub;
+typedef struct _component_port_sub _group_port_sub;
+struct _groupt_ports {
+    std::vector<_group_port_pub> pubs;
+    std::vector<_group_port_sub> subs;
+};
+
+/**
+ * Grouptype configuration
+ */
+struct _groupt_conf {
+    std::string   groupTypeId;
+    _groupt_ports groupTypePorts;
+};
+
 
 struct _component_conf {
     std::string        component_name;
@@ -69,8 +102,10 @@ struct _component_conf {
     _component_ports   component_ports;
     Parameters         component_parameters;
     bool               isDevice;
+    _groupt_conf       group_types;
 };
 
 typedef struct _component_conf component_conf;
+typedef struct _groupt_conf    groupt_conf;
 
 #endif //RIAPS_FW_R_CONFIGURATION_H
