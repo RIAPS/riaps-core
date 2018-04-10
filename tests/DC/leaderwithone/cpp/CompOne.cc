1f3fe21b736fe1292f3e6abc633853a08c2da0fe
istvan <istvan.madari@vanderbilt.edu>
2018-04-10 12:27:09 -0500

    test for leader election with one component added

---
+++ tests/DC/leaderwithone/cpp/CompOne.cc
@@ -0,0 +1,43 @@
+#include <CompOne.h>
+
+namespace groupmsgtest {
+   namespace components {
+      
+      CompOne::CompOne(_component_conf &config, riaps::Actor &actor) :
+      CompOneBase(config, actor), m_joinedToA(false) {
+          _logger->set_pattern("[%n] %v");
+      }
+      
+      void CompOne::OnClock(riaps::ports::PortBase *port) {
+         if (!m_joinedToA){
+             _logger->info("Component joins {}:{}", groupIdA.groupTypeId, groupIdA.groupName);
+            auto joined = JoinGroup(groupIdA);
+            if (joined){
+                m_joinedToA = true;
+            }
+            _logger->error_if(!joined, "Couldn't join group {}:{}", groupIdA.groupTypeId, groupIdA.groupName);
+         } else {
+
+             _logger->info("The leader is: {}", GetLeaderId(groupIdA));
+         }
+      }
+    
+      
+      void CompOne::OnGroupMessage(const riaps::groups::GroupId& groupId, capnp::FlatArrayMessageReader& capnpreader, riaps::ports::PortBase* port){
+
+      }
+      
+      CompOne::~CompOne() {
+         
+      }
+   }
+}
+
+riaps::ComponentBase *create_component(_component_conf &config, riaps::Actor &actor) {
+   auto result = new groupmsgtest::components::CompOne(config, actor);
+   return result;
+}
+
+void destroy_component(riaps::ComponentBase *comp) {
+   delete comp;
+}
