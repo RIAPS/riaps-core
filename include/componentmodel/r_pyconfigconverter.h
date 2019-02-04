b644a32820d36f95f7befde7270d2a6adbc7370b
istvan <istvan.madari@vanderbilt.edu>
2019-02-04 13:40:27 -0600

    fix: ports with local messages are not registered as local

--- include/componentmodel/r_pyconfigconverter.h
+++ include/componentmodel/r_pyconfigconverter.h
@@ -13,9 +13,8 @@ public:
     static ComponentConf convert(const py::dict& py_comp_config, const py::dict& py_actor) {
         ComponentConf result;
         result.is_device = false;
-
+        ParseLocals(py_actor);
         auto json_ports  = py_comp_config[J_PORTS];
-        auto json_locals = py_actor[J_LOCALS];
         auto json_pubs  = json_ports[J_PORTS_PUBS].cast<py::dict>();
         auto json_subs  = json_ports[J_PORTS_SUBS].cast<py::dict>();
         auto json_reqs  = json_ports[J_PORTS_REQS].cast<py::dict>();
@@ -33,6 +32,22 @@ public:
 
 private:
 
+    static std::set<std::string> locals_;
+
+    static void ParseLocals(const py::dict& py_actor) {
+        auto json_locals = py_actor[J_LOCALS];
+        for (auto it = json_locals.begin(); it!=json_locals.end(); it++){
+            locals_.insert(it->cast<py::dict>()["type"].cast<std::string>());
+        }
+    }
+
+    static bool IsLocal(const std::string& message_type) {
+        if (locals_.find(message_type) == locals_.end()) {
+            return false;
+        }
+        return true;
+    }
+
     // Todo: merge pub_sub and tim by enable_if
     template<class T>
     static void parse_pub_sub(const py::dict& ports, std::vector<T>& conf_container) {
@@ -41,6 +56,7 @@ private:
 
             port.port_name    = it->first.cast<std::string>();
             port.message_type = it->second.cast<py::dict>()[J_TYPE].cast<std::string>();
+            port.is_local     = IsLocal(port.message_type);
             conf_container.push_back(port);
         }
     };
@@ -52,6 +68,7 @@ private:
 
             port.req_type     = it->second[J_PORT_REQTYPE].cast<std::string>();
             port.rep_type     = it->second[J_PORT_REPTYPE].cast<std::string>();
+            port.is_local     = IsLocal(port.req_type) && IsLocal(port.rep_type);
             port.port_name    = it->first.cast<std::string>();
             port.message_type = port.req_type + "#" + port.rep_type;
             conf_container.push_back(port);
@@ -69,5 +86,7 @@ private:
         }
     }
 };
+
+std::set<std::string> PyConfigConverter::locals_ = {};
 
 #endif //RIAPS_CORE_CONVERTER_H
